ecf98864cab059b9d020403d1c2c5d699b616271
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-05-02 12:20:17 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacaoListas/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoListas
+{
+    internal class Employee
+    {
+        int id;
+        string name;
+        double salary;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            salary = salary + ((salary * percentage) / 100);
+        }
+    }
+}
---
+++ ExercicioFixacaoListas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ExercicioFixacaoListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            int id, qt, src;
+            double salary, percentage;
+            string name;
+
+            Console.Write("QUANTOS FUNCIONARIOS REGISTRADOS: ");
+            qt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qt; i++)
+            {
+                Console.Write("ID: ");
+                id = int.Parse(Console.ReadLine());
+
+                Console.Write("NOME: ");
+                name = Console.ReadLine();
+
+                Console.Write("SALARIO: ");
+                salary = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                employees.Add(new Employee(id, name, salary));
+
+            }
+
+            Console.WriteLine("Digite o ID para buscar: ");
+            src = int.Parse(Console.ReadLine());
+
+
+
+            foreach (Employee e in employees)
+            {
+                if(e.Id == src)
+                {
+                    Console.WriteLine("INSIRA O PERCENTUAL DE AUMENTO: ");
+                    percentage = double.Parse(Console.ReadLine());
+
+                    e.increaseSalary(percentage);
+                }
+            }
+            
+            foreach (Employee e in employees)
+            {
+                Console.WriteLine($"{e.Id}, {e.Name}, {e.Salary}");
+            }
+        }
+    }
+}
